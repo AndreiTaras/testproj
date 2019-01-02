53dbc82d142c856ac61773a432dc990970e3d138
Andrei Andeev <andrei_andreev_et@tut.by>
2019-01-03 01:01:11 +0300

    Add Base class

---
+++ src/main/java/com/test/overview/Base.java
@@ -0,0 +1,14 @@
+package com.test.overview;
+
+public class Base {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
